7e504405f810b1676e0550fe186496c8636bd583
timur anoshechkin <tima@quests-end.com>
2015-05-14 16:37:48 +1000

    - added debug UI menu

---
+++ StakeHolder Mapping/Assets/Scripts/DebugUI.cs
@@ -0,0 +1,39 @@
+﻿/** 
+ *	\brief Manager class which is responsible for Debug UI
+ *	\author Timur Anoshechkin
+ *	\copyright PenguinWolf
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class DebugUI : Manager<DebugUI>
+{
+    [SerializeField]
+    private GameObject _uIContainer = null;
+    private void Awake()
+    {
+        // Error checking
+        DebugUtils.Assert( _uIContainer != null, "Check that UIElementsContainer is hooked up" );
+
+        // Disable UIContainer on awake
+        _uIContainer.SetActive( false );
+    }
+
+    private void Update()
+    {
+        // Use "Tilde to enable/disbale debugUI"
+        {
+            if ( Input.GetKeyDown( KeyCode.BackQuote ) &&
+                 _uIContainer.activeSelf == false )
+            {
+                _uIContainer.SetActive( true );
+            }
+            else if ( Input.GetKeyDown( KeyCode.BackQuote ) &&
+                 _uIContainer.activeSelf == true )
+            {
+                _uIContainer.SetActive( false );
+            }
+        }
+    }
+}
