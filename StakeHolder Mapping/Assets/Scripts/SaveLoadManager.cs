0aa4fa21e958e4f07658edd7c923f39fc67cf29c
timur anoshechkin <tima@quests-end.com>
2015-05-14 14:52:35 +1000

    - Started POL-1 Similar implementation of save / screenshot - Started POL-5 Create manager for saving/loading scened and screesnhots - Moved utility scripts - Created SaveLoadManager class

---
+++ StakeHolder Mapping/Assets/Scripts/SaveLoadManager.cs
@@ -0,0 +1,13 @@
+﻿/** 
+ *	\brief Manager class which is responsible for Save/Load system implementation
+ *	\author Timur Anoshechkin
+ *	\copyright PenguinWolf
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class SaveLoadManager : Manager<SaveLoadManager>
+{
+
+}
